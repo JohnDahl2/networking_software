815fcc4ec71c116dbfcde2b3f0b5e9bd14b5e3f0
JohnDahl2 <john.dahl675@gmail.com>
2026-06-14 19:15:07 -0500

    clean up database sql

---
+++ go-sniffer/migrations/20260615001331_add_constraints_and_indexes.sql
@@ -0,0 +1,23 @@
+-- +goose Up
+
+-- Enforce that every job always has a status
+ALTER TABLE job_tracking
+    ALTER COLUMN status SET NOT NULL;
+
+-- Link packets to their job; cascade so DeleteJob cleans up automatically
+ALTER TABLE packet_logs
+    ADD CONSTRAINT fk_packet_logs_job_id
+    FOREIGN KEY (job_id) REFERENCES job_tracking(job_id)
+    ON DELETE CASCADE;
+
+-- Index job_id so filtering packets by job is fast
+CREATE INDEX idx_packet_logs_job_id ON packet_logs (job_id);
+
+-- +goose Down
+DROP INDEX IF EXISTS idx_packet_logs_job_id;
+
+ALTER TABLE packet_logs
+    DROP CONSTRAINT IF EXISTS fk_packet_logs_job_id;
+
+ALTER TABLE job_tracking
+    ALTER COLUMN status DROP NOT NULL;
